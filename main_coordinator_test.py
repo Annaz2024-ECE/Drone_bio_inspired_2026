dc072072cf1ee509d99c28a2328b3c279970b401
JessieShen59 <jessieshen59@outlook.com>
2026-06-28 12:15:25 +0800

    save: 再次临时保存本地修改

--- main_coordinator_test.py
+++ main_coordinator_test.py
@@ -8,7 +8,7 @@ from dsaco_planner import DSACOPlanner
 from pso_planner import PSOPlanner
 from gwo_planner import GWOPlanner
 from ssa_planner import SSAPlanner
-from woa_planner import WOAPlanner
+from woa_planner_fix import WOAPlannerArg
 
 def run_parameter_tuning_loop():
     print("=" * 60)
@@ -27,7 +27,7 @@ def run_parameter_tuning_loop():
         "PSO": PSOPlanner,
         "GWO": GWOPlanner,
         "SSA": SSAPlanner,
-        "WOA": WOAPlanner
+        "WOA": WOAPlannerArg
     }
     
     # 你只需修改这里！想测谁，就改成谁的名字
